e915c7a6f0fd571a917b325ff87734b737280515
Diyasha6432 <diyashabose2723@gmail.com>
2025-03-31 14:18:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello Diyasha')
+  res.send('Hello MCKV Diyasha')
 })
 
 app.get('/courses', function (req, res) {
